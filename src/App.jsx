f8dcf2f34b1a923a19f282c68acd90719843b1bb
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2025-08-08 02:22:35 +0530

    Added footer, but with some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./Header"
 import Main from "./MainContent"
+import Footer from "./Footer"
 
 export default function App() {
     return (
         <>
             <Header />
             <Main />
-            <Footer />
+            {<Footer />}
         </>
     )
 }
